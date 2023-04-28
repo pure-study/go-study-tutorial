133ef54af126033d95a9ae9e5ace1c3d038fcb76
Will <yzj_000@163.com>
2023-04-28 16:33:49 +0800

    Initial version

---
+++ fuzz/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"unicode/utf8"
+)
+
+func main() {
+	input := "The quick brown fox jumped over the lazy dog"
+	rev, revErr := Reverse(input)
+	doubleRev, doubleRevErr := Reverse(rev)
+	fmt.Printf("Original: %q\n", input)
+	fmt.Printf("Reversed: %q, err: %v\n", rev, revErr)
+	fmt.Printf("Reversed again: %q, err: %v\n", doubleRev, doubleRevErr)
+}
+
+func Reverse(s string) (string, error) {
+	if !utf8.ValidString(s) {
+		return s, errors.New("Input is not valid UTF-8")
+	}
+	b := []rune(s)
+	for i, j := 0, len(b)-1; i < len(b)/2; i, j = i+1, j-1 {
+		b[i], b[j] = b[j], b[i]
+	}
+	return string(b), nil
+}
